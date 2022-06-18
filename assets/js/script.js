951445f14ace76cb5621be4a0db9efa98b564bb6
Emilio <emiliothomasking@gmail.com>
2022-06-17 17:09:13 -0700

    commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+https://api.dictionaryapi.dev/api/v2/entries/en/<word>
+
+api.datamuse.com/words
+api.datamuse.com/sug //autocomplete function on search input fields
+
+/*
+rhy	-Rhymes ("perfect" rhymes, per RhymeZone)	spade → aid
+nry	-Approximate rhymes (per RhymeZone)	forest → chorus
+hom	-Homophones (sound-alike words)	course → coarse
+cns	-Consonant match
+syn	-Synonyms (words contained within the same WordNet synset)
+*/
+
+// supposed to make the textbox responsive
+$('#textarea1').val('New Text');
+M.textareaAutoResize($('#textarea1'));
+
